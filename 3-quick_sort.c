cff662471de16d51eb8de2e31e4f45326a7cca5f
Janice91 <Kouakou.janice.2002@gmail.com>
2025-12-12 21:59:24 +0000

    Add all sorting algorithm files for Holberton

---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * @array: The array to sort
+ * @low: Starting index
+ * @high: Ending index (pivot)
+ * @size: Size of the full array (for printing)
+ *
+ * Return: Final position of the pivot
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low;
+	int j, temp;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+
+	if (i != high)
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+
+	return (i);
+}
+
+/**
+ * quicksort_recursive - Recursively sorts array using quicksort
+ * @array: The array
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Size of array
+ */
+void quicksort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = lomuto_partition(array, low, high, size);
+		quicksort_recursive(array, low, pivot_index - 1, size);
+		quicksort_recursive(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers using Quick sort
+ * @array: The array to sort
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quicksort_recursive(array, 0, size - 1, size);
+}
---
+++ sort.h
@@ -0,0 +1,36 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Printing functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algorithms prototypes */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+/* Internal functions for quick sort (you can keep them in your .c too) */
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quicksort_recursive(int *array, int low, int high, size_t size);
+
+#endif /* SORT_H */
